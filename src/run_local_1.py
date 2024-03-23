539fe28bb5a8299d31055db2a1683bd57489f926
entbappy <entbappy73@gmail.com>
2024-03-23 12:39:27 +0600

    inference added

--- src/run_local_1.py
+++ src/run_local_1.py
@@ -10,7 +10,7 @@ B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
 instruction = "Convert the following text from English to Hindi: \n\n {text}"
 
 
-SYSTEM_PROMPT = B_SYS + DEFAULT_SYSTEM_PROMPT + E_SYS
+SYSTEM_PROMPT = B_SYS + CUSTOM_SYSTEM_PROMPT + E_SYS
 template = B_INST + SYSTEM_PROMPT + instruction + E_INST
 
 
